100a7874ed01676370fa1851be188ee57608ee9a
Hữu Quý <45280556+huuquy1211@users.noreply.github.com>
2020-04-17 22:15:26 +0700

    Update Matching

--- GameLearnEnlish/GameLearnEnlish/UserControls/Unit1Activity2UC.xaml.cs
+++ GameLearnEnlish/GameLearnEnlish/UserControls/Unit1Activity2UC.xaml.cs
@@ -24,5 +24,62 @@ namespace GameLearnEnlish.UserControls
         {
             InitializeComponent();
         }
+
+
+        #region [Question 1]
+        private void Question1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void Question1_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            imgGuyaudio1.Opacity = 0.7;
+            lblQuestion1.Opacity = 0.7;
+            imgNode1.Opacity = 0.7;
+        }
+
+        private void Question1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imgGuyaudio1.Opacity = 1;
+            lblQuestion1.Opacity = 1;
+            imgNode1.Opacity = 1;
+        }
+        #endregion
+
+        #region [Question 2]
+        private void Question2_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            imgGuyaudio2.Opacity = 0.7;
+            lblQuestion2.Opacity = 0.7;
+            imgNode2.Opacity = 0.7;
+        }
+
+        private void Question2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imgGuyaudio2.Opacity = 1;
+            lblQuestion2.Opacity = 1;
+            imgNode2.Opacity = 1;
+        }
+        #endregion
+
+        #region [Question 3]
+        private void Question3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imgGuyaudio3.Opacity = 0.7;
+            lblQuestion3.Opacity = 0.7;
+            imgNode3.Opacity = 0.7;
+        }
+        private void Question3_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imgGuyaudio3.Opacity = 1;
+            lblQuestion3.Opacity = 1;
+            imgNode3.Opacity = 1;
+           
+        }
+        #endregion
+
+       
     }
 }
